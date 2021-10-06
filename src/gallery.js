376c6c458de90c2c4f2d3be2575be14178ed7d15
anirudh <67142496+anirudh1601@users.noreply.github.com>
2021-10-06 18:51:48 +0530

    Update gallery.js

--- src/gallery.js
+++ src/gallery.js
@@ -4,8 +4,8 @@ import './gallery.css'
 
 import Gallery1 from "./gallery1"
 
-const Gallery = () =>{
-    
+const Gallery = (props) =>{
+    console.log(props.image)
     return (
         <>
     <h1 style={{"textAlign":'center',}}>Gallery</h1>
@@ -17,35 +17,68 @@ const Gallery = () =>{
             <div class="row">
 
                 <div class="col-sm-12 col-md-4">
-                < a className="lightbox" href="/gallery"><img src="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/bridge.jpg" alt="Bridge" /></a>
+                < a className="lightbox" href={props.image}><img src={props.image} alt="Bridge" /></a>
                         
                     
                 </div>
                 <div class="col-sm-6 col-md-4">
-                    <a class="lightbox" href="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/park.jpg">
-                        <img src="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/park.jpg" alt="Park" />
+                    <a class="lightbox" href={props.image1}>
+                        <img src={props.image1}alt="Park" />
                     </a>
                 </div>
                 <div class="col-sm-6 col-md-4">
-                    <a class="lightbox" href="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/tunnel.jpg">
-                        <img src="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/tunnel.jpg" alt="Tunnel" />
+                    <a class="lightbox" href={props.image2}>
+                        <img src={props.image2} alt="Tunnel" />
                     </a>
                 </div>
                 <div class="col-sm-12 col-md-8">
-                    <a class="lightbox" href="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/traffic.jpg">
-                        <img src="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/traffic.jpg" alt="Traffic" />
+                    <a class="lightbox" href={props.image3}>
+                        <img src={props.image3} alt="Traffic" />
                     </a>
                 </div>
                 <div class="col-sm-6 col-md-4">
-                    <a class="lightbox" href="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/rails.jpg">
-                        <img src="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/rails.jpg" alt="Coast" />
+                    <a class="lightbox" href={props.image4}>
+                        <img src={props.image4} alt="Coast" />
                     </a>
                 </div> 
-                <div class="col-sm-6 col-md-4">
-                    <a class="lightbox" href="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/coast.jpg">
-                        <img src="https://raw.githubusercontent.com/LeshikJanz/libraries/master/Related%20images/Bootstrap%20example/coast.jpg" alt="Rails" />
+                <div class="col-sm- col-md-4">
+                    <a class="lightbox" href={props.image5}>
+                        <img src={props.image5} alt="Rails" />
                     </a>
                 </div>
+                <div class="col-sm-14 col-md-4">
+                < a className="lightbox" href={props.image6}><img src={props.image6} alt="Bridge" /></a>
+                        
+                    
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href={props.image7}>
+                        <img src={props.image7} alt="Park" />
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href={props.image8}>
+                        <img src={props.image8} alt="Tunnel" />
+                    </a>
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <a class="lightbox" href={props.image9}>
+                        <img src={props.image9} alt="Traffic" />
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href={props.image10}>
+                        <img src={props.image10} alt="Coast" />
+                    </a>
+                </div> 
+                <div class="col-sm-8 col-md-4">
+                    <a class="lightbox" href={props.image11}>
+                        <img src={props.image11} alt="Rails" />
+                    </a>
+                </div>
+                
+                
+                
 
             </div>
 
